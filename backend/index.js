f8725918fe42dde57a983f1563f26e5a2efff309
Clemens Bruesch <clemensbruesch@gmail.com>
2021-04-17 16:02:18 +0200

    basic route working

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Hello from Codemon72')
+})
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server listening on port: ${PORT}`));
