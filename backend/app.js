96d1cb3bd011ec7924222e0d9d1bb68e97cbab5b
Clemens Bruesch <clemensbruesch@gmail.com>
2021-04-18 09:04:12 +0200

    code clean up

--- backend/app.js
+++ backend/app.js
@@ -15,36 +15,6 @@ const app = express();
 
 app.use(cors());
 
-// const mariadb = require("mariadb");
-
-// const pool = mariadb.createPool({
-//   host: "127.0.0.1",
-//   port: "3306",
-//   user: "clemens",
-//   database: "codingschool",
-//   password: "",
-//   connectionLimit: 5,
-// });
-
-// async function logAllCourses() {
-//   let conn;
-//   try {
-//     conn = await pool.getConnection();
-//     const courses = await conn.query({
-//       rowsAsArray: true,
-//       sql: "SELECT name FROM courses;",
-//     });
-//     courses.forEach((x) => console.log(x[0]));
-//   } catch (err) {
-//     throw err;
-//   } finally {
-//     if (conn) return conn.end();
-//   }
-// }
-
-// logAllCourses();
-
-
 // Route Home
 app.get("/", (req, res) => {
   res.send("Hello from Codemon72");
@@ -56,7 +26,6 @@ app.get("/courses", (req, res) => {
     include: [Teachers],
   })
     .then((courses) => {
-      // res.sendStatus(200);
       res.send(courses);
     })
     .catch((err) => console.log(`Error: ${err}`));
