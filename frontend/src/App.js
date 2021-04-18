bca4936ec57a88793db586ea99b733bb2d17a9f3
Clemens Bruesch <clemensbruesch@gmail.com>
2021-04-18 12:18:42 +0200

    client and server connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,28 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import Header from './components/Header'
 import Display from './components/Display'
 import Dashboard from './components/Dashboard'
+// import { BrowserRouter as Router, Route } from "react-router-dom"
 
 
 
 function App() {
-  
+  let [courses, setCourses] = useState([]);
+
+  const URLAllCourseData = 'http://localhost:4000/courses'
+
+  const fetchCourseData = async () => {
+    const res = await fetch(URLAllCourseData);
+    const data = await res.json();
+    setCourses(data);
+    console.log(data);
+    // setProducts(data.map((obj) => ({ ...obj, selected: true })));
+  };
+
+  // fetch course data on first rendering
   useEffect(() => {
-    console.log('Component mounted. ')
-  }, [])
+    fetchCourseData();
+  }, []);
 
   return (
     <>
