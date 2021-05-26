9689e564daaa53a620654b8395bd7577117f8f75
Clemens Bruesch <clemensbruesch@gmail.com>
2021-05-26 08:55:08 +0200

    course component iterates through sessions

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -23,6 +23,13 @@ const Course = ({course_event, handleDelete}) => {
       <div>End Date: {course_event.course_end_date}</div>
       <div>Hours: {course_event.Course_Module.hours}</div>
       <div>Trainer: { course_event.Teacher != null ? course_event.Teacher.name : 'not determined yet'}</div>
+      <div className="sessions">
+        <h4>Sessions</h4>
+      </div>
+      { course_event.Sessions.length === 0 && <span className="session">There are no sessions listed yet for this course.</span>}
+      { course_event.Sessions.map(session => {
+        return ( 'blib')
+      })}
 
       { !formVisibility && (
         <div className="button-box">
