35ed32859f8dea67ff95083e2d927b681094f426
Clemens Bruesch <clemensbruesch@gmail.com>
2021-05-28 16:09:09 +0200

    new AddSessions component

---
+++ client/src/components/AddSessions.js
@@ -0,0 +1,16 @@
+import { useState, useEffect, useContext } from 'react';
+import { CourseContext } from '../contexts/CourseContext';
+
+const AddSessions = () => {
+
+  
+  return (
+    <div>
+      <h3>Add Sessions</h3>
+
+      
+    </div>
+  )
+}
+
+export default AddSessions
