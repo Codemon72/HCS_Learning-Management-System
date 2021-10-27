222ad9f7d929e7e6e29094c3edc8b20a13e0bd5a
Clemens Bruesch <clemensbruesch@gmail.com>
2021-10-27 23:48:15 +0200

    clean up

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -20,16 +20,6 @@ const Course = ({course_event, handleDelete, fetchCourseData}) => {
             });
   };
 
-  const confirmDelete = () => {
-    window.addEventListener('click', function(e){
-      if (document.getElementById('clickbox').contains(e.target)){
-        alert("Clicked in Box");
-      } else{
-        alert("Clicked outside Box");
-      }
-    })
-  };
-
   const handleDeleteSession = (e, session_id) => {
     e.preventDefault();
     console.log('session_id: ', session_id);
