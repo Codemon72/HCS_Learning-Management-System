b59fb9df0c344a7460b4f8de19aeef40c558b284
Clemens Bruesch <clemensbruesch@gmail.com>
2021-11-07 18:40:05 +0100

    UpdateSessionForm displays full date but with 2hrs time off

--- client/src/components/UpdateSessionForm.js
+++ client/src/components/UpdateSessionForm.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
 
-const UpdateSessionForm = ({setFormVisibility, wipSession, displayDateTime}) => {
+const UpdateSessionForm = ({setFormVisibility, wipSession}) => {
 
   console.log('UpdateSessionForm rendered')
+  console.log('wipSession: ', wipSession)
+
+  const initialFormState = [
+    {
+      course_event_id: 'wipSession.course_event_id',
+      session_start: '',
+      session_end: '',
+    },
+  ];
+
+  const [formState, setFormState] = useState(initialFormState);
+
+
+  const handleSessionInputChange = (event) => {
+    let temp = [...formState];
+    const { name, value } = event.target;
+    temp.name = value;
+    temp.course_event_id = wipSession.course_event_id;
+    setFormState(temp);
+    console.table(temp);
+  };
+
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -10,9 +33,10 @@ const UpdateSessionForm = ({setFormVisibility, wipSession, displayDateTime}) => 
 
   return (
     <div className='update_form display__sessions'>
-      Edit Session
+      <h3>Edit Session #  {wipSession.index + 1}</h3>
+      <br />
       <form onSubmit={handleOnSubmit}>
-        <table>
+        {/* <table>
           <thead>
             <tr>
               <th>#</th>
@@ -31,7 +55,27 @@ const UpdateSessionForm = ({setFormVisibility, wipSession, displayDateTime}) => 
               <td>{displayDateTime(wipSession.session_end, 'time')}</td>
             </tr>
           </tbody>
-        </table>
+        </table> */}
+        <div className='input-group'>
+                <label htmlFor='session_start'>Start</label>
+                <input
+                  type='datetime-local'
+                  name='session_start'
+                  className='input-field'
+                  onChange={(e) => handleSessionInputChange(e)}
+                  value={wipSession.session_start.slice(0, -1)}
+                />
+              </div>
+              <div className='input-group'>
+                <label htmlFor='session_end'>End</label>
+                <input
+                  type='datetime-local'
+                  name='session_end'
+                  className='input-field'
+                  onChange={(e) => handleSessionInputChange(e)}
+                  value={wipSession.session_end.slice(0, -1)}
+                />
+              </div>
 
         <div className='button-box'>
           <button onClick={() => setFormVisibility('')}>Cancel</button>
