58d688938ae25e0f51e1103381f5653d5df2082f
Clemens Bruesch <clemensbruesch@gmail.com>
2021-11-09 19:09:59 +0100

    input fields change formState

--- client/src/components/UpdateSessionForm.js
+++ client/src/components/UpdateSessionForm.js
@@ -3,23 +3,20 @@ import { useState } from 'react';
 const UpdateSessionForm = ({setFormVisibility, wipSession}) => {
 
   console.log('UpdateSessionForm rendered')
-  console.log('wipSession: ', wipSession)
 
-  const initialFormState = [
-    {
-      course_event_id: 'wipSession.course_event_id',
-      session_start: '',
-      session_end: '',
-    },
-  ];
+  const initialFormState = {
+    course_event_id: '',
+    session_start: '',
+    session_end: '',
+  };
 
   const [formState, setFormState] = useState(initialFormState);
 
-
   const handleSessionInputChange = (event) => {
-    let temp = [...formState];
+    console.log('event.target: ', event.target)
+    let temp = {...formState};
     const { name, value } = event.target;
-    temp.name = value;
+    temp[name] = value;
     temp.course_event_id = wipSession.course_event_id;
     setFormState(temp);
     console.table(temp);
@@ -28,7 +25,14 @@ const UpdateSessionForm = ({setFormVisibility, wipSession}) => {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    console.log('formState: ', formState);
     console.log('on submit clicked')
+  };
+
+  // onlyDuringDev:
+  const logFormstate = (e) => {
+    e.preventDefault();
+    console.log(formState);
   };
 
   return (
@@ -36,26 +40,6 @@ const UpdateSessionForm = ({setFormVisibility, wipSession}) => {
       <h3>Edit Session #  {wipSession.index + 1}</h3>
       <br />
       <form onSubmit={handleOnSubmit}>
-        {/* <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Day</th>
-              <th>Date</th>
-              <th>Start</th>
-              <th>End</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className='display__session'>
-              <td>{wipSession.index + 1}</td>
-              <td>{displayDateTime(wipSession.session_start, 'weekday')}</td>
-              <td>{displayDateTime(wipSession.session_start, 'date')}</td>
-              <td>{displayDateTime(wipSession.session_start, 'time')}</td>
-              <td>{displayDateTime(wipSession.session_end, 'time')}</td>
-            </tr>
-          </tbody>
-        </table> */}
         <div className='input-group'>
                 <label htmlFor='session_start'>Start</label>
                 <input
@@ -63,7 +47,7 @@ const UpdateSessionForm = ({setFormVisibility, wipSession}) => {
                   name='session_start'
                   className='input-field'
                   onChange={(e) => handleSessionInputChange(e)}
-                  value={wipSession.session_start.slice(0, -1)}
+                  value={formState.session_start}
                 />
               </div>
               <div className='input-group'>
@@ -73,7 +57,7 @@ const UpdateSessionForm = ({setFormVisibility, wipSession}) => {
                   name='session_end'
                   className='input-field'
                   onChange={(e) => handleSessionInputChange(e)}
-                  value={wipSession.session_end.slice(0, -1)}
+                  value={formState.session_end}
                 />
               </div>
 
@@ -82,6 +66,13 @@ const UpdateSessionForm = ({setFormVisibility, wipSession}) => {
           <input type='submit' className='button' />
         </div>
       </form>
+      {/* onlyDuringDev: */}
+        <br />
+        <div className='addSessionFormField'>
+          <button className='dashboard__session button' onClick={logFormstate}>
+            (Log FormState)
+          </button>
+        </div>
     </div>
   );
 }
